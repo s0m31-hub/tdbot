4e5b79cc2dfc3269894e7f2daab48ee87c1eed0b
s0m31 <notlimitedwolf676@gmail.com>
2022-07-14 22:00:20 +0300

    Fix !upgrade command

--- src/main/java/org/nwolfhub/UpdateHandler.java
+++ src/main/java/org/nwolfhub/UpdateHandler.java
@@ -134,7 +134,7 @@ public class UpdateHandler {
         } else if (text.toLowerCase().contains("!reverse")) {
             String[] split = text.split(" ");
             if(split[0].equals("!reverse") && split.length > 1) {
-                String textToInverse = text.replace("!reverse", "");
+                String textToInverse = text.replace("!reverse ", "");
                 request.chatId=chatId;
                 request.messageId=update.message.id;
                 request.inputMessageContent = new TdApi.InputMessageText(new TdApi.FormattedText(reverse(textToInverse), new TdApi.TextEntity[0]), false, false);
@@ -172,7 +172,7 @@ public class UpdateHandler {
         } else {
             String[] split = text.split(" ");
             if(split[0].equalsIgnoreCase("!mix") || split[0].equalsIgnoreCase("!broke")) {
-                request.chatId = chatId; request.messageId = update.message.id; request.inputMessageContent = new TdApi.InputMessageText(new TdApi.FormattedText(brokenText(text.replace("!mix", "").replace("!broke", "")), new TdApi.TextEntity[0]), false, false);
+                request.chatId = chatId; request.messageId = update.message.id; request.inputMessageContent = new TdApi.InputMessageText(new TdApi.FormattedText(brokenText(text.replace("!mix ", "").replace("!broke ", "")), new TdApi.TextEntity[0]), false, false);
                 client.send(request, UpdateHandler::requestFailHandler);
             }
         }
@@ -225,7 +225,7 @@ public class UpdateHandler {
                 request.inputMessageContent = new TdApi.InputMessageText(new TdApi.FormattedText("Request sent. Processing...", new TdApi.TextEntity[0]), false, false);
                 client.send(request, UpdateHandler::requestFailHandler);
                 try {
-                    String oldText = text.replace("!continue", "");
+                    String oldText = text.replace("!continue ", "");
                     String newText = continueText(oldText);
                     request.chatId = update.message.chatId;
                     request.messageId = update.message.id;
@@ -244,6 +244,12 @@ public class UpdateHandler {
     public static void upgradeText(String text, TdApi.UpdateNewMessage update) {
         TdApi.EditMessageText request = new TdApi.EditMessageText();
         String[] split = text.split(" ");
+        if(split.length==1) {
+            request.chatId=update.message.chatId;
+            request.messageId=update.message.id;
+            request.inputMessageContent = new TdApi.InputMessageText(new TdApi.FormattedText("Usage: !upgrade pattern", new TdApi.TextEntity[0]), false, false);
+            client.send(request, UpdateHandler::requestFailHandler);
+        }
         if(split.length==2 && split[0].equalsIgnoreCase("!upgrade")) {
             if(update.message.replyToMessageId == 0) {
                 request.chatId=update.message.chatId;
@@ -266,7 +272,7 @@ public class UpdateHandler {
         } else if(split.length>2 && split[0].equalsIgnoreCase("!upgrade")) {
             request.chatId = update.message.chatId;
             request.messageId = update.message.id;
-            String oldText = text.replace("!upgrade " + split[1], "");
+            String oldText = text.replace("!upgrade " + split[1] + " ", "");
             if(patterns.contains(split[1].toLowerCase())) {
                 request.inputMessageContent = new TdApi.InputMessageText(new TdApi.FormattedText(upgradeText(oldText, split[1].toLowerCase()), new TdApi.TextEntity[0]), false, false);
             } else {
@@ -280,27 +286,28 @@ public class UpdateHandler {
         var CYRILLIC_TO_LATIN = "Russian-Latin/BGN";
         Transliterator toLatinTrans = Transliterator.getInstance(CYRILLIC_TO_LATIN);
         String translate = toLatinTrans.transliterate(text).toLowerCase();
+        System.out.println(translate);
         StringBuilder result = new StringBuilder();
         List<String> english = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
         switch (pattern) {
             case "bubbles": {
                 List<String> another = Arrays.asList("\uD83C\uDD50 \uD83C\uDD51 \uD83C\uDD52 \uD83C\uDD53 \uD83C\uDD54 \uD83C\uDD55 \uD83C\uDD56 \uD83C\uDD57 \uD83C\uDD58 \uD83C\uDD59 \uD83C\uDD5A \uD83C\uDD5B \uD83C\uDD5C \uD83C\uDD5D \uD83C\uDD5E \uD83C\uDD5F \uD83C\uDD60 \uD83C\uDD61 \uD83C\uDD62 \uD83C\uDD63 \uD83C\uDD64 \uD83C\uDD65 \uD83C\uDD66 \uD83C\uDD67 \uD83C\uDD68 \uD83C\uDD69".split(" "));
                 for(String character:translate.split("")) {
-                    if(english.contains(character)) result.append(another.get(result.indexOf(character)));
+                    if(english.contains(character)) result.append(another.get(english.indexOf(character)));
                     else result.append(character);
                 }
                 break;
             } case "squares": {
                 List<String> another = Arrays.asList("\uD83C\uDD70 \uD83C\uDD71 \uD83C\uDD72 \uD83C\uDD73 \uD83C\uDD74 \uD83C\uDD75 \uD83C\uDD76 \uD83C\uDD77 \uD83C\uDD78 \uD83C\uDD79 \uD83C\uDD7A \uD83C\uDD7B \uD83C\uDD7C \uD83C\uDD7D \uD83C\uDD7E \uD83C\uDD7F \uD83C\uDD80 \uD83C\uDD81 \uD83C\uDD82 \uD83C\uDD83 \uD83C\uDD84 \uD83C\uDD85 \uD83C\uDD86 \uD83C\uDD87 \uD83C\uDD88 \uD83C\uDD89".split(" "));
                 for(String character:translate.split("")) {
-                    if(english.contains(character)) result.append(another.get(result.indexOf(character)));
+                    if(english.contains(character)) result.append(another.get(english.indexOf(character)));
                     else result.append(character);
                 }
                 break;
             } case "gothic": {
                 List<String> another = Arrays.asList("\uD835\uDD86 \uD835\uDD87 \uD835\uDD88 \uD835\uDD89 \uD835\uDD8A \uD835\uDD8B \uD835\uDD8C \uD835\uDD8D \uD835\uDD8E \uD835\uDD8F \uD835\uDD90 \uD835\uDD91 \uD835\uDD92 \uD835\uDD93 \uD835\uDD94 \uD835\uDD95 \uD835\uDD96 \uD835\uDD97 \uD835\uDD98 \uD835\uDD99 \uD835\uDD9A \uD835\uDD9B \uD835\uDD9C \uD835\uDD9D \uD835\uDD9E \uD835\uDD9F".split(" "));
                 for(String character:translate.split("")) {
-                    if(english.contains(character)) result.append(another.get(result.indexOf(character)));
+                    if(english.contains(character)) result.append(another.get(english.indexOf(character)));
                     else result.append(character);
                 }
                 break;
@@ -308,10 +315,11 @@ public class UpdateHandler {
                 List<String> another = Arrays.asList("\uD835\uDCEA \uD835\uDCEB \uD835\uDCEC \uD835\uDCED \uD835\uDCEE \uD835\uDCEF \uD835\uDCF0 \uD835\uDCF1 \uD835\uDCF2 \uD835\uDCF3 \uD835\uDCF4 \uD835\uDCF5 \uD835\uDCF6 \uD835\uDCF7 \uD835\uDCF8 \uD835\uDCF9 \uD835\uDCFA \uD835\uDCFB \uD835\uDCFC \uD835\uDCFD \uD835\uDCFE \uD835\uDCFF \uD835\uDD00 \uD835\uDD01 \uD835\uDD02 \uD835\uDD03".split(" "));
                 result.append("༺ ");
                 for(String character:translate.split("")) {
-                    if(english.contains(character)) result.append(another.get(result.indexOf(character)));
+                    if(english.contains(character)) result.append(another.get(english.indexOf(character)));
                     else result.append(character);
                 }
                 result.append(" ༻");
+                break;
             }
         }
         return result.toString();
@@ -332,6 +340,7 @@ public class UpdateHandler {
                     }
                     if(text.contains("!mix") || text.contains("!broke")) mix(text, update);
                     if(text.contains("!continue")) continueText(text, update);
+                    if(text.contains("!upgrade")) upgradeText(text, update);
                 }
             } else {
                 long sender = 0;
